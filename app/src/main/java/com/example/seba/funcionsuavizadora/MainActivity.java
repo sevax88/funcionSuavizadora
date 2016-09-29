052ac182760a82fe79591a617a1d09f4367e6948
SebasMuller <sebastian.muller@tictapps.com>
2016-09-29 16:13:05 -0300

    con 4 zonas de interes

--- app/src/main/java/com/example/seba/funcionsuavizadora/MainActivity.java
+++ app/src/main/java/com/example/seba/funcionsuavizadora/MainActivity.java
@@ -18,21 +18,22 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView rssiBeacon1,rssiBeacon2,rssiBeacon3,candidato,toptv;
+    private TextView rssiBeacon1,rssiBeacon2,rssiBeacon3,rssiBeacon4,candidato,toptv;
     private BeaconManager beaconManager;
     private Region region;
     private int listenerCount = 0;
     private Map<Integer,Integer> readsBc,beaconsSoporte;
     private double alpha = 0.1;
-    private TextView soporteAmarillo,soporteCandy,soporteRemolacha,equipoAmarillotv,equipoCandytv,equipoRemolachatv;
+    private TextView soporteAmarillo,soporteCandy,soporteRemolacha,equipoAmarillotv,equipoCandytv,equipoRemolachatv,equipoVerdetv,soporteVerde;
     private Integer rssiCarry;
     private int equipoAmarillo;
     private int equipoCandy;
-    private int equipoRemolacha;
+    private int equipoRemolacha,equipoVerde;
 
 
     @Override
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         beaconManager = new BeaconManager(this);
 //        beaconManager.setBackgroundScanPeriod(250,0);
         beaconManager.setForegroundScanPeriod(950,0);
-        region = new Region("ranged region", UUID.fromString("E7CD3AEB-363F-9EF0-4E53-DFB7DC5DDD46"), null, null);
+        region = new Region("ranged region", null, null, null);
         beaconManager.setRangingListener(new BeaconManager.RangingListener() {
             @Override
             public void onBeaconsDiscovered(Region region, final List<Beacon> list) {
@@ -72,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
                                             break;
                                         case 1731:
                                             rssiBeacon3.setText("rssi remolacha1 - 1731 = " + actualRssi);
+                                            break;
+                                        case 4739:
+                                            rssiBeacon4.setText("rssi verde1 - 4739 = " + actualRssi);
                                             break;
                                         default:
                                             break;
@@ -120,50 +124,49 @@ public class MainActivity extends AppCompatActivity {
                                             }
                                             equipoRemolachatv.setText("equipo remolacha = " + String.valueOf(equipoRemolacha));
                                             break;
+                                        case 13451:
+                                            soporteVerde.setText("rssi soporte verde = " + actualRssi);
+                                            rssiCarry = readsBc.get(4739);
+                                            equipoVerde = rssiCarry + actualRssi;
+                                            if (Math.abs(rssiCarry - actualRssi)>10){
+                                                equipoVerde = equipoVerde - 10;
+                                            }
+                                            if (Math.abs(rssiCarry - actualRssi)<10){
+                                                equipoVerde = equipoVerde - 5;
+                                            }
+                                            equipoVerdetv.setText("equipo verde = " + String.valueOf(equipoVerde));
+                                            break;
                                         default:
                                             break;
                                     }
                                 }
                             }
-                            if (equipoAmarillo < equipoCandy) {
-                                if (equipoAmarillo < equipoRemolacha) {
-//                                    if (equipoAmarillo < 132) {
-                                        toptv.setText("AND THE AMI GOES TO EQUIPO AMARILLO");
-//                                    } else {
-//                                        toptv.setText("ESTAS EN EL MEDIO");
-//                                    }
-                                } else {
-//                                    if (equipoRemolacha < 132) {
-                                        toptv.setText("AND THE AMI GOES TO EQUIPO REMOLACHA");
-//                                    } else {
-//                                        toptv.setText("ESTAS EN EL MEDIO");
-//                                    }
-                                }
-                            } else if (equipoCandy < equipoRemolacha) {
-//                                if (equipoCandy < 132) {
-                                    toptv.setText("AND THE AMI GOES TO EQUIPO CANDY");
-//                                } else {
-//                                    toptv.setText("ESTAS EN EL MEDIO");
-//                                }
-                            } else {
-//                                if (equipoRemolacha < 132) {
-                                    toptv.setText("AND THE AMI GOES TO EQUIPO REMOLACHA");
-//                                } else {
-//                                    toptv.setText("ESTAS EN EL MEDIO");
-//                                }
-                            }
-                            equipoAmarillo = 0;
-                            equipoRemolacha = 0;
-                            equipoCandy = 0;
-                            //llamar a la funcion que ordena el map by values
-//                            toptv.setText("AND THE AMI GOES TO... " + sortHashMapByValues((HashMap<Integer, Integer>) readsBc));
+                           //meterlos en una lista y ordenarlos , sacar el primero
+                            Map<Integer,String> equiposMap = new TreeMap<Integer, String>();
+                            equiposMap.put(equipoRemolacha,"equipo Remolacha");
+                            equiposMap.put(equipoCandy, "equipo Candy");
+                            equiposMap.put(equipoAmarillo, "equipoAmarillo");
+                            equiposMap.put(equipoVerde, "equipo Verde");
+                            String equipoGanador = equiposMap.values().toArray()[0].toString();
+                            toptv.setText("AND THE AMI GOES FOR " + equipoGanador);
+                            resetearEquipos();
+                            equiposMap.clear();
+
                         }
                     });
-                    // TODO: update the UI here
                 }
             }
         });
 
+
+
+    }
+
+    private void resetearEquipos() {
+        equipoAmarillo = 0;
+        equipoRemolacha = 0;
+        equipoCandy = 0;
+        equipoVerde = 0;
     }
 
     private void fillMap() {
@@ -172,55 +175,60 @@ public class MainActivity extends AppCompatActivity {
 
         readsBc.put(28695, 0);          //minor del lemon1
         readsBc.put(1731, 0);           //minor del remolacha1
-        readsBc.put (52909,0);   //minor candy1
+        readsBc.put (52909,0);          //minor candy1
+        readsBc.put (4739,0);           //minor verde1
 
-        beaconsSoporte.put(28617,0);           //minor del lemon2
+        beaconsSoporte.put(28617,0);    //minor del lemon2
         beaconsSoporte.put (27802,0);   //minor candy2
         beaconsSoporte.put (25989,0);   //minor remolacha2
-
+        beaconsSoporte.put(13451,0);    //minor del verde2
     }
 
     private void initViews() {
         rssiBeacon1 = (TextView)findViewById(R.id.rssiBeacon1);
         rssiBeacon2 = (TextView)findViewById(R.id.rssiBeacon2);
         rssiBeacon3 = (TextView)findViewById(R.id.rssiBeacon3);
+        rssiBeacon4 = (TextView)findViewById(R.id.rssiBeacon4);
         soporteAmarillo = (TextView)findViewById(R.id.soporteAmarillo);
         soporteCandy = (TextView)findViewById(R.id.soporteCandy);
         soporteRemolacha = (TextView)findViewById(R.id.soporteRemolacha);
+        soporteVerde = (TextView)findViewById(R.id.soporteVerde);
 
         equipoAmarillotv = (TextView) findViewById(R.id.equipoAmarillo);
         equipoCandytv = (TextView)findViewById(R.id.equipoCandy);
         equipoRemolachatv = (TextView)findViewById(R.id.equipoRemolacha);
+        equipoVerdetv = (TextView)findViewById(R.id.equipoVerde);
 
         toptv = (TextView)findViewById(R.id.toptv);
     }
 
-    public int sortHashMapByValues(HashMap<Integer, Integer> passedMap) {
+    public LinkedHashMap<Integer, String> sortHashMapByValues(HashMap<Integer, String> passedMap) {
         List<Integer> mapKeys = new ArrayList<>(passedMap.keySet());
-        List<Integer> mapValues = new ArrayList<>(passedMap.values());
-        Collections.sort(mapValues);
+        List<String> mapValues = new ArrayList<>(passedMap.values());
         Collections.sort(mapKeys);
+//        Collections.sort(mapKeys);
 
-        LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
+        LinkedHashMap<Integer, String> sortedMap =
+                new LinkedHashMap<>();
 
-        Iterator<Integer> valueIt = mapValues.iterator();
+        Iterator<String> valueIt = mapValues.iterator();
         while (valueIt.hasNext()) {
-            int val = valueIt.next();
+            String val = valueIt.next();
             Iterator<Integer> keyIt = mapKeys.iterator();
 
             while (keyIt.hasNext()) {
                 Integer key = keyIt.next();
-                Integer comp1 = passedMap.get(key);
-                Integer comp2 = val;
+                String comp1 = passedMap.get(key);
+                String comp2 = val;
 
-                if (comp1==comp2) {
+                if (comp1.equals(comp2)) {
                     keyIt.remove();
                     sortedMap.put(key, val);
                     break;
                 }
             }
         }
-        return (int) sortedMap.keySet().toArray()[0];
+        return sortedMap;
     }
 
     @Override
