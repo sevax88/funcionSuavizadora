80424eab51b84119cfa1b74ded7c4ea3ec22c0e2
SebasMuller <sebastian.muller@tictapps.com>
2016-10-18 16:41:53 -0300

    correcion del application

--- app/src/main/java/com/example/seba/funcionsuavizadora/MyApplication.java
+++ app/src/main/java/com/example/seba/funcionsuavizadora/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.seba.funcionsuavizadora;
 
 import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
 
 import com.estimote.sdk.EstimoteSDK;
@@ -8,14 +9,15 @@ import com.estimote.sdk.EstimoteSDK;
 /**
  * Created by seba on 19/09/2016.
  */
-public class MyApplication extends Activity {
+public class MyApplication extends Application {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate() {
+        super.onCreate();
         //  App ID & App Token can be taken from App section of Estimote Cloud.
-        EstimoteSDK.initialize(getApplicationContext(), "com-example-seba-funcionsu-7kw", "bd21e09a23281f9817b176c24713bdea");
+//        EstimoteSDK.initialize(getApplicationContext(), "com-example-seba-funcionsu-7kw", "bd21e09a23281f9817b176c24713bdea");
 // Optional, debug logging.
-        EstimoteSDK.enableDebugLogging(true);
+//        EstimoteSDK.enableDebugLogging(true);
     }
+
 }
